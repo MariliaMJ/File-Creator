960047dafed5f7711348bb6efdbdba839096d723
MaríliaMorais <mmjanizelli@gmail.com>
2020-10-29 22:38:59 -0300

    feat: Turn into a Mix project

---
+++ thoth/lib/thoth.ex
@@ -0,0 +1,18 @@
+defmodule Thoth do
+  @moduledoc """
+  Documentation for `Thoth`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Thoth.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
